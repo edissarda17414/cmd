4c4d995b89778f22c8c79cae68abe1f5a4bd66a0
edissarda17414 <edissarda@yahoo.com>
2017-03-06 23:14:44 +0100

    CMD Test Java

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,14 +13,11 @@ import java.io.IOException;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws IOException {
         System.out.println("pocetak");
-        
+
         Process p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && ping localhost && echo end\"");
-        
+
         System.out.println("kraj");
     }
 
